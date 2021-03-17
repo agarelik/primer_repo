ec4491cfd526eac982dcb2c68874b32995c43d08
arielgarelik <agarelik@hotmail.com>
2021-03-17 10:16:06 -0300

    tercer commit

--- BSEO001.cbl
+++ BSEO001.cbl
@@ -11,7 +11,7 @@
        IDENTIFICATION DIVISION.
       *=======================*
        PROGRAM-ID. BSEO001.
-author. yo.
+       AUTHOR. ARIEL GARELIK.
       *====================*
        ENVIRONMENT DIVISION.
       *====================*
