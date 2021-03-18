d32359e4deb428d29ddad6cd98ceb6bab84e6a8a
arielgarelik <ariel@hotmail.com>
2021-03-17 23:59:29 -0300

    agregado tutor

--- BSEO001.cbl
+++ BSEO001.cbl
@@ -14,6 +14,7 @@
        AUTHOR. ARIEL GARELIK.
        colegio. sek.
        curso. 8f.
+       tutor. simon.
       *====================*
        ENVIRONMENT DIVISION.
       *====================*
